8687032c94edae60481000edf05c513e6570a838
Adam Kenawell <adam.kenawell.family@gmail.com>
2026-05-07 12:13:38 -0400

    Fix: add trailing slash to base URL

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://adam-kenawell.github.io',
-  base: '/adam.kenawell.family',
+  base: '/adam.kenawell.family/',
 });
